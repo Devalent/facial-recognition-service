2afc07be77047dfe8835cf38b2fdc6536e9b5b6d
Artem Abashev <artem@abashev.com>
2022-02-05 20:17:03 +0300

    Add Redux.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux'
+
+import store from '../store';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.scss';
@@ -6,7 +9,11 @@ import 'bootstrap-icons/font/bootstrap-icons.scss';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp;
